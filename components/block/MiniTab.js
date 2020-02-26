fdc8ed2383cd302a4fce654da481235e3a75871c
Hung Do <hung.do@miracles.vn>
2020-02-26 17:58:30 +0700

    fix MiniTab

--- components/block/MiniTab.js
+++ components/block/MiniTab.js
@@ -11,26 +11,23 @@ function MiniTab({ data }) {
   return (
     <section className="sec-tb">
       <h2 className="ht">{data[0].title}</h2>
-
       <div className="cttab-v3   ">
         <div className="tab-menu">
+          {map(data, value => (
+            <div className="active">
+              <span>{value.tabtitle}</span>
+            </div>
+          ))}
+        </div>
+        <div className="tab-content">
           <div className="active">
             {map(data, value => (
-              <div className="active">
-                <span>{value.tabTitle}</span>
+              <div className="tab-inner">
+                <div className="list-7  list-item row">
+                  <MiniTabItem data={value} />
+                </div>
               </div>
             ))}
-            <div className="tab-content">
-              <div className="active">
-                {map(data, value => (
-                  <div className="tab-inner">
-                    <div className="list-7  list-item row">
-                      <MiniTabItem data={value} />
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </div>
           </div>
         </div>
       </div>
