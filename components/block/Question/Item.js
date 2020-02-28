bebda2ea9c38bddefbc0aa6845cb0b46d96d740d
Vũ Việt Anh <haidangvu07@gmail.com>
2020-02-28 17:47:53 +0700

    fix html block question

--- components/block/Question/Item.js
+++ components/block/Question/Item.js
@@ -11,20 +11,17 @@ function QuestionItems({ question, answer }) {
   const [active, setAcive] = useState(false);
 
   return (
-    <div className="accodion accodion-1">
-        <div className="accodion-tab question_wapper mb-3">
-        <div className={!active ? 'question_wapperTitle' : 'question_wapperTitle active'} className="accodion-title">
-        <div className="title_content container" onClick={() => setAcive(!active)}>
-          <div className="rectangle">{!active ? <div className="icon-plus-2"/> : <div className="icon-minus-2"/>}</div>
-          <div className="accodion-content entry-content">
-            <div class="inner">
-              <p>{question}</p>
-            </div>
-          </div> 
-        </div>
+    <div className="accodion-tab ">
+      <input type="checkbox" checked={active} />
+      <label className="accodion-title" onClick={() => setAcive(!active)}>
+        <span>{question}</span>
+        <span className="triangle">
+          <i className="icon-plus"></i>
+        </span>
+      </label>
+      <div className="accodion-content entry-content">
+        <div className="inner">{ReactHtmlParser(answer)}</div>
       </div>
-      {active && <div className="answer container">{ReactHtmlParser(answer)}</div>}
-    </div>
     </div>
   );
 }
