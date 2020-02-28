1235eed2d128c1c986deffd6617e0b769cce0436
Quyet Pham <quyet.pham@miracles.vn>
2020-02-28 14:13:29 +0700

    Add icon in QuestionItems

--- components/block/Question/Item.js
+++ components/block/Question/Item.js
@@ -11,18 +11,20 @@ function QuestionItems({ question, answer }) {
   const [active, setAcive] = useState(false);
 
   return (
-    <div className="question_wapper mb-3">
-      <div className={!active ? 'question_wapperTitle' : 'question_wapperTitle active'}>
+    <div className="accodion accodion-1">
+        <div className="accodion-tab question_wapper mb-3">
+        <div className={!active ? 'question_wapperTitle' : 'question_wapperTitle active'} className="accodion-title">
         <div className="title_content container" onClick={() => setAcive(!active)}>
-          <div className="icon">
-            <div className="rectangle">{!active ? `+` : `-`}</div>
-          </div>
-          <div className="text">
-            <p>{question}</p>
-          </div>
+          <div className="rectangle">{!active ? <div className="icon-plus-2"/> : <div className="icon-minus-2"/>}</div>
+          <div className="accodion-content entry-content">
+            <div class="inner">
+              <p>{question}</p>
+            </div>
+          </div> 
         </div>
       </div>
       {active && <div className="answer container">{ReactHtmlParser(answer)}</div>}
+    </div>
     </div>
   );
 }
