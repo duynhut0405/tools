76ed1d1455322de945fa979a4bf78145f7d3117d
Hung Do <hung.do@miracles.vn>
2020-02-28 09:24:38 +0700

    Fix News

--- components/block/News.js
+++ components/block/News.js
@@ -8,17 +8,17 @@ const propTypes = {
   type: Proptypes.number
 };
 
-function Post({ data, type }) {
+function News({ data, type }) {
   const [page, setPage] = useState(3);
   const [active, setActive] = useState(false);
   const listNews = slice(data.news, 0, page);
+  const listNewsTabs = slice(data.news, 2, 5);
   //   slice(data.news, 0, page)
   useEffect(() => {
     if (listNews.length === data.news.length) {
       setActive(true);
     }
   }, [page]);
-
   const showPage = () => {
     setActive(false);
     setPage(page + 3);
@@ -27,39 +27,89 @@ function Post({ data, type }) {
       window.scrollTo({ top: 0, behavior: 'smooth' });
     }
   };
- 
-  return (
-    <div className="post_block mb-5 pt-4 mt-5">
-      <div className="title">
-        <h2>{data.title}</h2>
-      </div>
-      <div className="row">
-        {map(listNews, item => (
-          <div className="col-sm-4 col-md-6 col-lg-4" key={item.newsId}>
-            <div className="post_content mb-3">
-              <div style={{ height: '230px', width: '100%', overflow: 'hidden' }}>
-                <img src={item.base_image} alt="icon" />
-              </div>
-              <div className="content">
-                <div className="date mt-4">
-                  <p>{moment(item.created_at).format('DD-MM-YYYY')}</p>
+  if (type === '1') {
+    return (
+      <div className="post_block mb-5 pt-4 mt-5">
+        <div className="title">
+          <h2>{data.title}</h2>
+        </div>
+        <div className="row">
+          {map(listNews, item => (
+            <div className="col-sm-4 col-md-6 col-lg-4" key={item.newsId}>
+              <div className="post_content mb-3">
+                <div style={{ height: '230px', width: '100%', overflow: 'hidden' }}>
+                  <img src={item.base_image} alt="icon" />
                 </div>
-                <p className="title">{item.title}</p>
-                <p className="show">
-                  <a href={`/news/${item.url}`}>Xem thêm</a>
-                </p>
+                <div className="content">
+                  <div className="date mt-4">
+                    <p>{moment(item.created_at).format('DD-MM-YYYY')}</p>
+                  </div>
+                  <p className="title">{item.title}</p>
+                  <p className="show">
+                    <a href={`/news/${item.url}`}>Xem thêm</a>
+                  </p>
+                </div>
               </div>
             </div>
+          ))}
+        </div>
+        <div className="btn">
+          <button onClick={() => showPage()}>{active === false ? 'Xem thêm' : 'Thu gọn'}</button>
+        </div>
+      </div>
+    );
+  }
+  if (type === '3') {
+    return (
+      <section className="sec-tb sec-h-4">
+        <div className="container">
+          <div className="entry-head">
+            <h2 className="ht efch-1 ef-img-l">{data.title}</h2>
+            <a className="viewall" href="#">
+              Xem tất cả <i className="icon-arrow-1"></i>
+            </a>
           </div>
-        ))}
-      </div>
-      <div className="btn">
-        <button onClick={() => showPage()}>{active === false ? 'Xem thêm' : 'Thu gọn'}</button>
-      </div>
-    </div>
-  );
+          {/* 2tabs main */}
+          <div className="row list-item">
+            <div className="col-lg-8 ">
+              <div className="list-5 row ">
+                {map(listNews, item => (
+                  <div className="col-md-6">
+                    <a href="#" className="item efch-<?php echo $i+1; ?> ef-img-l equal">
+                      <div className="img tRes_71">
+                        <img className="lazy-hidden" data-lazy-type="image" src={item.base_image} />
+                      </div>
+                      <div className="divtext">
+                        <div className="date">{moment(item.created_at).format('DD-MM-YYYY')}</div>
+                        <h4 className="title line2">{item.title}</h4>
+                        <div className="desc line2">{item.description}</div>
+                      </div>
+                    </a>
+                  </div>
+                ))}
+              </div>
+            </div>
+            {/* 3tabs */}
+            <div className="col-lg-4">
+              {map(listNewsTabs, item => (
+                <div className="list-6">
+                  <a href="#" className="item item-inline-table">
+                    <div className="img">
+                      <img className="lazy-hidden" data-lazy-type="image" src={item.base_image} />
+                    </div>
+                    <div className="divtext">
+                      <h4 className="title line4">{item.title}</h4>
+                    </div>
+                  </a>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
+News.propTypes = propTypes;
 
-Post.propTypes = propTypes;
-
-export default Post;
+export default News;
