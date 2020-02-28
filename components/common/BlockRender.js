1f3280e5b035480756a74cbaa0ad46ea0b46f830
Dong Le <dong.le@miracles.vn>
2020-02-28 09:25:18 +0700

    fix conflic

--- components/common/BlockRender.js
+++ components/common/BlockRender.js
@@ -22,56 +22,6 @@ const propTypes = {
 function BlockRender({ data }) {
   return (
     <>
-<<<<<<< HEAD
-      {map(data, (values, index) => {
-        if (values.name === 'Block Icon') {
-          return <Icon data={JSON.parse(values.content)} key={index} />;
-        }
-        if (values.name === 'Block Images') {
-          return <Images data={JSON.parse(values.content)} key={index} />;
-        }
-        if (values.name === 'Single Post') {
-          return <SingerPost data={JSON.parse(values.content)} key={index} />;
-        }
-        if (values.name === 'Block News') {
-          return (
-            <News
-              type={JSON.parse(values.content).type}
-              data={JSON.parse(values.content)}
-              key={index}
-            />
-          );
-        }
-        if (values.name === 'Pages') {
-          return <Pages data={JSON.parse(values.content)} key={index} />;
-        }
-        if (values.name === 'MiniTab') {
-          return <MiniTab data={JSON.parse(values.content)} key={index} />;
-        }
-        if (values.name === 'Repeat') {
-          return <Repeat data={JSON.parse(values.content)} key={index} />;
-        }
-        if (values.name === 'Product') {
-          return <Product data={JSON.parse(values.content)} key={index} />;
-        }
-        if (values.name === 'Question') {
-          return <Question data={JSON.parse(values.content)} key={index} />;
-        }
-        if (values.name === 'Block Pages') {
-          return (
-            <Pages
-              type={JSON.parse(values.content).type}
-              data={JSON.parse(values.content)}
-              key={index}
-            />
-          );
-        }
-        if (values.name === 'Tab Mini') {
-          return <MiniTab data={JSON.parse(values.content)} key={index} />;
-        }
-        return null;
-      })}
-=======
       {data !== undefined && (
         <>
           {map(
@@ -97,12 +47,6 @@ function BlockRender({ data }) {
               }
               if (values.name === 'Block News') {
                 return <Post data={JSON.parse(values.content)} key={index} />;
-              }
-              if (values.name === 'Post LayOut') {
-                return <PostLayOut data={JSON.parse(values.content)} key={index} />;
-              }
-              if (values.name === 'Promotion') {
-                return <Promotion data={JSON.parse(values.content)} key={index} />;
               }
               if (values.name === 'Pages') {
                 return <Pages data={JSON.parse(values.content)} key={index} />;
@@ -136,7 +80,6 @@ function BlockRender({ data }) {
           )}
         </>
       )}
->>>>>>> e8f2916080e7c9e3371062cc2cb9cbb06a6b001e
     </>
   );
 }
