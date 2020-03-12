b52ac2ec95e0af270f9951a59b5084dd3bf1e6c7
Vũ Việt Anh <haidangvu07@gmail.com>
2020-03-12 10:36:27 +0700

    fix lib export map

--- next.config.js
+++ next.config.js
@@ -19,29 +19,29 @@ module.exports = withFonts(
         });
         return config;
       },
-      // exportPathMap: async function() {
-      //   const res = await getRouer();
-      //   const newResponse = await getNewRouter();
-      //   const newRouter = newResponse.data.reduce(
-      //     (pages, data) =>
-      //       Object.assign({}, pages, {
-      //         [`/news/${data.url}`]: { page: '/news/[...slug]' }
-      //       }),
-      //     {}
-      //   );
+      exportPathMap: async function() {
+        const res = await getRouer();
+        const newResponse = await getNewRouter();
+        const newRouter = newResponse.data.reduce(
+          (pages, data) =>
+            Object.assign({}, pages, {
+              [`/news/${data.url}`]: { page: '/news/[...slug]' }
+            }),
+          {}
+        );
 
-      //   const router = res.data.reduce(
-      //     (pages, data) =>
-      //       Object.assign({}, pages, {
-      //         [`/page/${data.slug}`]: { page: '/page/[...name]' }
-      //       }),
-      //     {}
-      //   );
-      //   const pageRouter = Object.assign({}, router, {
-      //     '/': { page: '/' }
-      //   });
-      //   return Object.assign(newRouter, pageRouter);
-      // }
+        const router = res.data.reduce(
+          (pages, data) =>
+            Object.assign({}, pages, {
+              [`/page/${data.slug}`]: { page: '/page/[...name]' }
+            }),
+          {}
+        );
+        const pageRouter = Object.assign({}, router, {
+          '/': { page: '/' }
+        });
+        return Object.assign(newRouter, pageRouter);
+      }
     })
   )
 );
