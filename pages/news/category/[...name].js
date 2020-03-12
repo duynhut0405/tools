85faea83d5d9ea0e4f27893ec305da27affa8919
Vũ Việt Anh <haidangvu07@gmail.com>
2020-03-12 10:59:04 +0700

    fix token get page get news

--- pages/news/category/[...name].js
+++ pages/news/category/[...name].js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Layout from '../../../components/layout';
 
 function CategoryDetail() {
-  return <div>vanh123</div>;
+  return (
+    <Layout>
+      <h1> vanh</h1>
+    </Layout>
+  );
 }
 
 export default CategoryDetail;
