940a68430e66aaa5fcf0172b384cc618be2577b7
Hung Do <hung.do@miracles.vn>
2020-03-14 13:15:14 +0700

    fix api router

--- components/block/News.js
+++ components/block/News.js
@@ -42,8 +42,6 @@ function News({ data, type, id }) {
     }
   };
 
-  console.log(data);
-
   const getCategoryById = async () => {
     const res = await getCategoryByIdService(Number(data.category.value));
     if (res && res.status === 200) {
