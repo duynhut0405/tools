042aafd7f8f3a21506a6a8b2d68b84bb70f1cd50
Hung Do <hung.do@miracles.vn>
2020-03-18 01:11:24 +0700

    add type and fix image Center

--- components/block/Images/Center.js
+++ components/block/Images/Center.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import ReactHtmlParser from 'react-html-parser';
 
@@ -7,44 +7,118 @@ const propTypes = {
 };
 
 function ImageCenter({ items }) {
-  return (
-    <section className="sec-tb sec-cskh block-images-center">
-      <div className="container">
-        <div className="entry-head text-center">
-          {ReactHtmlParser(items.note_1)}
-          {ReactHtmlParser(items.note_2)}
-          {ReactHtmlParser(items.note_3)}
+  const [active, setActive] = useState(false);
+
+  if (!items.background || items.background === '1') {
+    return (
+      <section className="sec-tb sec-cskh block-images-center">
+        <div className="container">
+          <div className="bgvideo loaded">
+            <div className="entry-head text-center">
+              {ReactHtmlParser(items.note_1)}
+              {ReactHtmlParser(items.note_2)}
+              {ReactHtmlParser(items.note_3)}
+            </div>
+            {!active && (
+              <div
+                className="single_video  tRes_16_9 max750"
+                data-id="2UrWPUAr68A"
+                data-video="autoplay=1&amp;controls=1&amp;mute=0"
+              >
+                <img
+                  className=" loaded loaded"
+                  data-lazy-type="image"
+                  data-lazy-src={items.image}
+                  src={items.image}
+                  alt=""
+                />
+                <span
+                  className="btnvideo"
+                  onClick={() => {
+                    if (items.text_action === undefined || items.text_action === '') {
+                      setActive(!active);
+                    }
+                  }}
+                >
+                  <i className="icon-play"></i>
+                </span>
+              </div>
+            )}
+            {active && (
+              <div
+                className="single_video  tRes_16_9 max750"
+                data-id="2UrWPUAr68A"
+                data-video="autoplay=1&amp;controls=1&amp;mute=0"
+              >
+                <iframe
+                  frameBorder="0"
+                  allowFullScreen="1"
+                  allow="autoplay; encrypted-media;"
+                  src={items.video_url}
+                ></iframe>
+              </div>
+            )}
+          </div>
         </div>
-        {(items.video_url === undefined || items.video_url === '') && (
-          <div className="img tRes_66 efch-2 ef-img-r ">
-            <img
-              className=" loaded loaded"
-              data-lazy-type="image"
-              data-lazy-src={items.image}
-              src={items.image}
-            />
+      </section>
+    );
+  }
+  if (items.background === '2') {
+    return (
+      <React.Fragment>
+        <section className=" sec-tb sec-cskh-2 ">
+          <div className="container">
+            <div className="bgvideo loaded">
+              <div className="entry-head text-center">
+                {ReactHtmlParser(items.note_1)}
+                {ReactHtmlParser(items.note_2)}
+                {ReactHtmlParser(items.note_3)}
+              </div>
+              {!active && (
+                <div
+                  className="single_video  tRes_16_9 max750"
+                  data-id="2UrWPUAr68A"
+                  data-video="autoplay=1&amp;controls=1&amp;mute=0"
+                >
+                  <img
+                    className=" loaded loaded"
+                    data-lazy-type="image"
+                    data-lazy-src={items.image}
+                    src={items.image}
+                    alt=""
+                  />
+                  <span
+                    className="btnvideo"
+                    onClick={() => {
+                      if (items.text_action === undefined || items.text_action === '') {
+                        setActive(!active);
+                      }
+                    }}
+                  >
+                    <i className="icon-play"></i>
+                  </span>
+                </div>
+              )}
+              {active && (
+                <div
+                  className="single_video  tRes_16_9 max750"
+                  data-id="2UrWPUAr68A"
+                  data-video="autoplay=1&amp;controls=1&amp;mute=0"
+                >
+                  <iframe
+                    frameBorder="0"
+                    allowFullScreen="1"
+                    allow="autoplay; encrypted-media;"
+                    src={items.video_url}
+                  ></iframe>
+                </div>
+              )}
+            </div>
           </div>
-        )}
-        {items.video_url !== undefined && items.video_url !== '' && (
-          <div className="single_video  tRes_16_9 max750">
-            <iframe src={items.video_url} />
-          </div>
-        )}
-        {/* {items.text_action !== undefined && (
-          <a className="btn lg" href={items.url === undefined ? '#' : items.url}>
-            {items.text_action === undefined ? 'Đăng ký ngay' : items.text_action}
-          </a>
-        )} */}
-        {items.text_action === undefined || items.text_action === '' ? (
-          ''
-        ) : (
-          <a className="btn lg" href={items.url === undefined ? '#' : items.url}>
-            {items.text_action === undefined ? 'Đăng ký ngay' : items.text_action}
-          </a>
-        )}
-      </div>
-    </section>
-  );
+        </section>
+      </React.Fragment>
+    );
+  }
 }
 
 ImageCenter.propTypes = propTypes;
