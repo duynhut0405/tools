1617cfb31465f8f1db8a0cc78f7dc08ded371c8e
Hung Do <hung.do@miracles.vn>
2020-03-18 17:49:32 +0700

    add Block Investor , OtherNews, LisDownloadFIle, ListDownloadVIdeo , Transaction

--- pages/page/[...name].js
+++ pages/page/[...name].js
@@ -1,10 +1,6 @@
 import React, { useEffect } from 'react';
 import { Carousel, BlockRender, MenuMiddle, DowloadCategory } from '../../components/common';
-import { ListDowloadFIle, ListDowloadVideo } from '../../components/common/download';
 import Layout from '../../components/layout';
-// import About from '../../components/about';
-import Investors from '../../components/investors';
-import OtherNews from '../../components/otherNews';
 import Transaction from '../../components/transaction';
 import { map } from 'lodash';
 import { PageActions } from '../../store/actions';
@@ -38,16 +34,6 @@ function Page({ list, silder, menuMiddle, getPage }) {
       <div className="main_content">
         <Carousel silder={silder} />
         <MenuMiddle data={menuMiddle} query={routerUrl} />
-        {/* {list.id === 34652 && <Investors />} */}
-        {list.id === 34653 && <Investors />}
-        {list.id === 34654 && <ListDowloadFIle type={1} search />}
-        {list.id === 34655 && <ListDowloadVideo type={2} />}
-        {list.id === 34656 && <ListDowloadVideo type={3} />}
-        {list.id === 34657 && <ListDowloadFIle type={4} search />}
-        {list.id === 34658 && <ListDowloadFIle type={6} search />}
-        {list.id === 34660 && <OtherNews />}
-        {/* {list.id === 34217 && <About />} */}
-        {list.id === 34669 && <Transaction />}
         {list.id === 34670 && <Contact />}
         <BlockRender data={list.pageBlocks} />
         {list.template === 4 && <DowloadCategory />}
