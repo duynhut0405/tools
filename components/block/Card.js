ba810845c0f86caf6a9c7dc3f19e10aad01ddcf2
Dong Le <dong.le@miracles.vn>
2020-03-18 11:07:16 +0700

    add card no image

--- components/block/Card.js
+++ components/block/Card.js
@@ -8,9 +8,34 @@ const propTypes = {
 };
 
 function Card({ data }) {
-  console.log(data);
   return (
     <React.Fragment>
+      {data[0].type === '3' && (
+        <React.Fragment>
+          <section className="sec-tb sec-img-svg-3 group-ef loaded">
+            <div className="container">
+              <div className="row equalHeight list-item">
+                {map(data, (items, index) => {
+                  return (
+                    <div className={`col-sm-6 col-md-4 efch-2 ef-img-t `}>
+                      <div className="item">
+                        <div className="divtext" style={{ paddingTop: '29px' }}>
+                          <h4 className="title equal" style={{ textAlign: 'center' }}>
+                            {ReactHtmlParser(items.note_1)}
+                          </h4>
+                          <div className="desc" style={{ textAlign: 'center' }}>
+                            {ReactHtmlParser(items.note_2)}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </section>
+        </React.Fragment>
+      )}
       {data[0].type === '4' && (
         <React.Fragment>
           <section className="sec-tb sec-img-svg-3 group-ef loaded">
