d88e2bc2f8aa44ce7d3fbf2c6c49e8ccea5e9e91
Dong Le <dong.le@miracles.vn>
2020-03-24 11:43:52 +0700

    add block category

--- components/block/Pages.js
+++ components/block/Pages.js
@@ -13,7 +13,9 @@ const propTypes = {
 function Pages({ data, type, id }) {
   // const [page] = useState(3);
   const [listPage, setListPage] = useState([]);
+  const [refCarouselTwo, setRefCarouselTwo] = useState(null);
   const [refCarouselThree, setRefCarouselThree] = useState(null);
+  const [active, setActive] = useState(false);
   const getPageBlock = async () => {
     const ids = map(data.pages, values => values.value);
     const res = await getPagesByIdService(ids);
@@ -21,6 +23,8 @@ function Pages({ data, type, id }) {
       setListPage(res.data);
     }
   };
+  console.log(listPage);
+  console.log(data);
   const responsiveTwo = {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
@@ -228,6 +232,81 @@ function Pages({ data, type, id }) {
                   }}
                 >
                   <i className="icon-arrow-1"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+  if (type && type === '6') {
+    return (
+      <section className=" sec-b sec-cauhoi ">
+        <div className="container">
+          <div className="entry-head">
+            <h2 className="ht ">{data === null ? '' : data.title}</h2>
+          </div>
+          <div className="accodion accodion-1">
+            <div className="accodion-tab ">
+              <input type="checkbox" checked={active} />
+              <label className="accodion-title" onClick={() => setActive(!active)}>
+                <span>{data === null ? '' : data.description}</span>
+                <span className="triangle">
+                  <i className="icon-plus"></i>
+                </span>
+              </label>
+              <div className="accodion-content entry-content">
+                <div className="owl-carousel equalHeight s-nav nav-2 list-5 owl-loaded owl-drag ">
+                  <div className="owl-stage-outer">
+                    <div className="owl-stage">
+                      <Carousel
+                        responsive={responsiveTwo}
+                        draggable
+                        minimumTouchDrag={80}
+                        ssr={true} // means to render carousel on server-side.
+                        infinite={true}
+                        keyBoardControl={true}
+                        arrows={false}
+                        ref={ref => {
+                          setRefCarouselTwo(ref);
+                        }}
+                      >
+                        {map(listPage, (items, index) => (
+                          <div className="item ef-img-t item_carousel" key={index}>
+                            <a href={`/page/${items.slug}`} className="link">
+                              <div className="img">
+                                <img src={items.baseImage} />
+                              </div>
+                              <div className="divtext">
+                                <h4 className="title line2">{items.name}</h4>
+                                <div className="desc line2 cl3">{items.meta_description}</div>
+                              </div>
+                            </a>
+                          </div>
+                        ))}
+                      </Carousel>
+                    </div>
+                  </div>
+                  <div className="owl-nav">
+                    <div
+                      className="owl-prev disabled"
+                      onClick={() => {
+                        refCarouselTwo.previous();
+                      }}
+                    >
+                      <i className="icon-arrow-1 ix"></i>
+                    </div>
+                    <div
+                      className="owl-next"
+                      onClick={() => {
+                        refCarouselTwo.next();
+                      }}
+                    >
+                      <i className="icon-arrow-1"></i>
+                    </div>
+                  </div>
+                  <div className="owl-dots disabled"></div>
                 </div>
               </div>
             </div>
