f996bcf5e63f8dd23b390e34dd9ce97ce9e4bef1
Anh <viet.anh@miracles.vn>
2020-03-24 10:06:49 +0700

    fix mneu midd

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const withSass = require('@zeit/next-sass');
 const withCSS = require('@zeit/next-css');
 const withFonts = require('nextjs-fonts');
-// const { getRouer, getNewRouter, getCategoryRouter } = require('./services/router');
+const { getRouer, getNewRouter, getCategoryRouter } = require('./services/router');
 
 module.exports = withFonts(
   withCSS(
@@ -18,43 +18,43 @@ module.exports = withFonts(
           }
         });
         return config;
+      },
+      exportPathMap: async function() {
+        const res = await getRouer();
+
+        const router = res.data.reduce(
+          (pages, data) =>
+            Object.assign({}, pages, {
+              [`/page/${data.slug}`]: { page: '/page/[...name]' }
+            }),
+          {}
+        );
+
+        const newResponse = await getNewRouter();
+        const newRouter = newResponse.data.reduce(
+          (pages, data) =>
+            Object.assign({}, pages, {
+              [`/news/${data.url}`]: { page: '/news/[...slug]' }
+            }),
+          {}
+        );
+
+        const categoryResponse = await getCategoryRouter();
+        const categoryRouter = categoryResponse.data.reduce(
+          (pages, category) =>
+            Object.assign({}, pages, {
+              [`/news/category/${category.slug}`]: { page: '/news/category/[...name]' }
+            }),
+          {}
+        );
+
+        let pageRouter = Object.assign({}, router, {
+          '/': { page: '/' }
+        });
+
+        pageRouter = Object.assign(pageRouter, categoryRouter);
+        return Object.assign(newRouter, pageRouter);
       }
-      // exportPathMap: async function() {
-      //   const res = await getRouer();
-
-      //   const router = res.data.reduce(
-      //     (pages, data) =>
-      //       Object.assign({}, pages, {
-      //         [`/page/${data.slug}`]: { page: '/page/[...name]' }
-      //       }),
-      //     {}
-      //   );
-
-      //   const newResponse = await getNewRouter();
-      //   const newRouter = newResponse.data.reduce(
-      //     (pages, data) =>
-      //       Object.assign({}, pages, {
-      //         [`/news/${data.url}`]: { page: '/news/[...slug]' }
-      //       }),
-      //     {}
-      //   );
-
-      //   const categoryResponse = await getCategoryRouter();
-      //   const categoryRouter = categoryResponse.data.reduce(
-      //     (pages, category) =>
-      //       Object.assign({}, pages, {
-      //         [`/news/category/${category.slug}`]: { page: '/news/category/[...name]' }
-      //       }),
-      //     {}
-      //   );
-
-      //   let pageRouter = Object.assign({}, router, {
-      //     '/': { page: '/' }
-      //   });
-
-      //   pageRouter = Object.assign(pageRouter, categoryRouter);
-      //   return Object.assign(newRouter, pageRouter);
-      // }
     })
   )
 );
