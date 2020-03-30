1ec2b350a1822a449820d8139a4d7f775d22caaa
Hung Do <hung.do@miracles.vn>
2020-03-31 00:20:01 +0700

    fix slugPage Menu

--- components/layout/ModalDrawer/submenu.js
+++ components/layout/ModalDrawer/submenu.js
@@ -27,7 +27,7 @@ function SubMenu({ menu }) {
             }}
           ></span>
         )}
-        <a href={`/page/${item.slugPages.value}`}>
+        <a href={`/page/${item.slugPages}`}>
           <span>{item.name}</span>
         </a>
         {item.children.length > 0 && (
