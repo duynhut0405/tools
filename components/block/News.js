4414d776e2ff822b2726bf5b1156c827509353eb
Quoc Toan <toancuchi@gmail.com>
2020-03-30 16:18:00 +0700

    Mango - update css

--- components/block/News.js
+++ components/block/News.js
@@ -176,49 +176,68 @@ function News({ data, type, id }) {
               </a>
             </div>
           )}
-
-          <Carousel
-            responsive={responsive}
-            draggable
-            minimumTouchDrag={80}
-            ssr={true} // means to render carousel on server-side.
-            infinite={true}
-            keyBoardControl={true}
-            arrows={true}
-            renderButtonGroupOutside={true}
-            className="list-5"
-            ref={ref => {
-              setRefCarousel(ref);
-            }}
-          >
-            {map(listCategory, (item, index) => (
-              <div className="slide-item" key={index}>
-                <a
-                  href={`/news/${item.url}`}
-                  className={`item efch-${index} ef-img-l `}
-                  key={index}
-                  //style={{ height: '300px', width: '262px' }}
-                >
-                  <div className="img tRes_71">
-                    <img
-                      className="lazy-hidden"
-                      data-lazy-type="image"
-                      src={item.base_image}
-                      style={{ height: '187px' }}
-                    />
-                  </div>
-                  <div className="divtext">
-                    <div className="date">{moment(item.created_at).format('DD-MM-YYYY')}</div>
-                    <h4 className="title">
-                      <ShowMoreText lines={1} more="" expanded={false} width={370}>
-                        {item.title}
-                      </ShowMoreText>
-                    </h4>
-                  </div>
-                </a>
+          <div className="wrap-carousel">
+            <Carousel
+              responsive={responsive}
+              draggable
+              minimumTouchDrag={80}
+              ssr={true} // means to render carousel on server-side.
+              infinite={true}
+              keyBoardControl={true}
+              arrows={false}
+              renderButtonGroupOutside={true}
+              className="list-5"
+              ref={ref => {
+                setRefCarousel(ref);
+              }}
+            >
+              {map(listCategory, (item, index) => (
+                <div className="slide-item" key={index}>
+                  <a
+                    href={`/news/${item.url}`}
+                    className={`item efch-${index} ef-img-l `}
+                    key={index}
+                    //style={{ height: '300px', width: '262px' }}
+                  >
+                    <div className="img tRes_71">
+                      <img
+                        className="lazy-hidden"
+                        data-lazy-type="image"
+                        src={item.base_image}
+                        style={{ height: '187px' }}
+                      />
+                    </div>
+                    <div className="divtext">
+                      <div className="date">{moment(item.created_at).format('DD-MM-YYYY')}</div>
+                      <h4 className="title">
+                        <ShowMoreText lines={1} more="" expanded={false} width={370}>
+                          {item.title}
+                        </ShowMoreText>
+                      </h4>
+                    </div>
+                  </a>
+                </div>
+              ))}
+            </Carousel>
+            <div className="carousel-nav center">
+              <div
+                className="carousel-prev "
+                onClick={() => {
+                  refCarousel.previous();
+                }}
+              >
+                <i className="icon-arrow-1 ix"></i>
               </div>
-            ))}
-          </Carousel>
+              <div
+                className="carousel-next"
+                onClick={() => {
+                  refCarousel.next();
+                }}
+              >
+                <i className="icon-arrow-1"></i>
+              </div>
+            </div>            
+          </div>
           {(data.title === undefined || data.title === '') && (
             <div className="text-center mt-4">
               <a className="btn lg" href={`/news/category/${slugCategory}`}>
