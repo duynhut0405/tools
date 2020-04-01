930756d64d585ea7bcc2c8117d6d1c1c46b79619
Hung Do <hung.do@miracles.vn>
2020-04-01 14:47:23 +0700

    fix block About

--- components/about/index.js
+++ components/about/index.js
@@ -48,12 +48,12 @@ function About({ data, listNews, getNews }) {
               <option value={2021}>2021</option>
             </select>
           </div>
-          <div className="list-5 row list-item">
+          <div className="list-12 row list-item">
             {map(listNews.news, item => {
               return (
                 <div className="col-md-4" key={item.newsId}>
                   <a href={`/news/${item.url}`} className="item efch-2 ef-img-l equal">
-                    <div className="img tRes_71">
+                    <div className="img">
                       <img
                         className=" loaded loaded"
                         data-lazy-type="image"
@@ -62,9 +62,8 @@ function About({ data, listNews, getNews }) {
                       />
                     </div>
                     <div className="divtext">
-                      <div className="date">{moment(item.created_at).format('DD/MM/YYYY')}</div>
+                      <div className="cl6">Ngành ngân hàng</div>
                       <h4 className="title line2">{item.title}</h4>
-                      <div className="desc line2">{item.shortDescription}</div>
                     </div>
                   </a>
                 </div>
