16a1497478c1b2278767b3e55a1959f310004a07
Hung Do <hung.do@miracles.vn>
2020-04-01 14:29:54 +0700

    fix block TabPayment

--- components/block/TablePayment.js
+++ components/block/TablePayment.js
@@ -7,15 +7,15 @@ const propTypes = {
   data: Proptypes.array
 };
 
-function TablePayment({ data }) {
+function TablePayment({ data, id }) {
   return (
     <section className="sec-tb">
       <div className="container">
         <div className="accodion accodion-2 ">
           {map(data, (items, index) => (
             <div className="accodion-tab ">
-              <input type="checkbox" id={`chck_${index}_${index + 1}`} />
-              <label className="accodion-title h2" htmlFor={`chck_${index}_${index + 1}`}>
+              <input type="checkbox" id={`chck_${id}_${index}`} />
+              <label className="accodion-title h2" htmlFor={`chck_${id}_${index}`}>
                 <span>{items.title}</span>
                 <span className="triangle">
                   <i className="icon-plus"></i>
