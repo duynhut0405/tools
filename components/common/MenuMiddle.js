e8ad055f0d1df9ad0cf39c79c772203924c1752a
Anh <viet.anh@miracles.vn>
2020-04-16 18:16:41 +0700

    fix paination

--- components/common/MenuMiddle.js
+++ components/common/MenuMiddle.js
@@ -28,101 +28,132 @@ function MenuMiddle({ data, query }) {
   return (
     <>
       {data !== null && (
-        <div className="sec-tb">
-          <div className="container">
-            <div className="text-center common-menumiddle entry-head">
-              <h1 className="ht">{data.title}</h1>
-            </div>
-            <div className="wrap-carousel max850">
-              {data.menuItems !== undefined && data.menuItems.length >= 6 && (
-                <Carousel
-                  responsive={responsive}
-                  draggable
-                  minimumTouchDrag={80}
-                  ssr={true} // means to render carousel on server-side.
-                  infinite={true}
-                  arrows={false}
-                  className="menuicon"
-                  keyBoardControl={true}
-                  ref={ref => {
-                    setRefCarousel(ref);
-                  }}
-                >
-                  {map(
-                    data.menuItems.sort((a, b) => a.position - b.position),
-                    (item, key) => {
-                      return (
-                        <div
-                          className={query === item.slugPages ? 'item active' : 'item'}
-                          key={key}
-                        >
-                          <a href={`/page/${item.slugPages}`} className="link">
-                            <div className="img">
-                              <img
-                                className=" loaded loaded"
-                                data-lazy-type="image"
-                                data-lazy-src={`${process.env.DOMAIN}${item.icon}`}
-                                src={`${process.env.DOMAIN}${item.icon}`}
-                              />
-                            </div>
-                            <div className="title">{item.name}</div>
-                          </a>
-                        </div>
-                      );
-                    }
-                  )}
-                </Carousel>
-              )}
-              {data.menuItems !== undefined && data.menuItems.length >= 6 && (
-                <div className="carousel-nav center">
-                  <div
-                    className="carousel-prev "
-                    onClick={() => {
-                      refCarousel.previous();
+        <React.Fragment>
+          <section className="sec-tb shop-pc">
+            <div className="container">
+              <div className="text-center common-menumiddle entry-head">
+                <h1 className="ht">{data.title}</h1>
+              </div>
+              <div className="wrap-carousel max850">
+                {data.menuItems !== undefined && data.menuItems.length >= 6 && (
+                  <Carousel
+                    responsive={responsive}
+                    draggable
+                    minimumTouchDrag={80}
+                    ssr={true} // means to render carousel on server-side.
+                    infinite={true}
+                    arrows={false}
+                    className="menuicon"
+                    keyBoardControl={true}
+                    ref={ref => {
+                      setRefCarousel(ref);
                     }}
                   >
-                    <i className="icon-arrow-1 ix"></i>
+                    {map(
+                      data.menuItems.sort((a, b) => a.position - b.position),
+                      (item, key) => {
+                        return (
+                          <div
+                            className={query === item.slugPages ? 'item active' : 'item'}
+                            key={key}
+                          >
+                            <a href={`/page/${item.slugPages}`} className="link">
+                              <div className="img">
+                                <img
+                                  className=" loaded loaded"
+                                  data-lazy-type="image"
+                                  data-lazy-src={`${process.env.DOMAIN}${item.icon}`}
+                                  src={`${process.env.DOMAIN}${item.icon}`}
+                                />
+                              </div>
+                              <div className="title">{item.name}</div>
+                            </a>
+                          </div>
+                        );
+                      }
+                    )}
+                  </Carousel>
+                )}
+                {data.menuItems !== undefined && data.menuItems.length >= 6 && (
+                  <div className="carousel-nav center">
+                    <div
+                      className="carousel-prev "
+                      onClick={() => {
+                        refCarousel.previous();
+                      }}
+                    >
+                      <i className="icon-arrow-1 ix"></i>
+                    </div>
+                    <div
+                      className="carousel-next"
+                      onClick={() => {
+                        refCarousel.next();
+                      }}
+                    >
+                      <i className="icon-arrow-1"></i>
+                    </div>
                   </div>
-                  <div
-                    className="carousel-next"
-                    onClick={() => {
-                      refCarousel.next();
-                    }}
-                  >
-                    <i className="icon-arrow-1"></i>
+                )}
+                {data.menuItems !== undefined && data.menuItems.length < 6 && (
+                  <div className="list-menu-middle__ menuicon">
+                    {map(
+                      data.menuItems.sort((a, b) => a.position - b.position),
+                      (item, key) => {
+                        return (
+                          <div
+                            className={query === item.slugPages ? 'item active' : 'item'}
+                            key={key}
+                          >
+                            <a href={`/page/${item.slugPages}`} className="link">
+                              <div className="img">
+                                <img
+                                  className=" loaded loaded"
+                                  data-lazy-type="image"
+                                  data-lazy-src={`${process.env.DOMAIN}${item.icon}`}
+                                  src={`${process.env.DOMAIN}${item.icon}`}
+                                />
+                              </div>
+                              <div className="title">{item.name}</div>
+                            </a>
+                          </div>
+                        );
+                      }
+                    )}
                   </div>
-                </div>
-              )}
-              {data.menuItems !== undefined && data.menuItems.length < 6 && (
-                <div className="list-menu-middle__ menuicon">
-                  {map(
-                    data.menuItems.sort((a, b) => a.position - b.position),
-                    (item, key) => {
-                      return (
-                        <div
-                          className={query === item.slugPages ? 'item active' : 'item'}
-                          key={key}
-                        >
-                          <a href={`/page/${item.slugPages}`} className="link">
-                            <div className="img">
-                              <img
-                                className=" loaded loaded"
-                                data-lazy-type="image"
-                                data-lazy-src={`${process.env.DOMAIN}${item.icon}`}
-                                src={`${process.env.DOMAIN}${item.icon}`}
-                              />
-                            </div>
-                            <div className="title">{item.name}</div>
-                          </a>
-                        </div>
-                      );
-                    }
-                  )}
-                </div>
-              )}
+                )}
+              </div>
             </div>
-          </div>
-        </div>
+          </section>
+          <section className="sec-tb sec-h-1 show-mb">
+            <div className="container">
+              <div className="entry-head text-center">
+                <h2 className="ht efch-1 ef-img-t">{data.title}</h2>
+              </div>
+              <div className="menuicon">
+                {map(
+                  data.menuItems.sort((a, b) => a.position - b.position),
+                  (item, key) => {
+                    return (
+                      <div className={query === item.slugPages ? 'item active' : 'item'} key={key}>
+                        <a href={`/page/${item.slugPages}`} className="link">
+                          <div className="img">
+                            <img
+                              className=" loaded loaded"
+                              data-lazy-type="image"
+                              data-lazy-src={`${process.env.DOMAIN}${item.icon}`}
+                              src={`${process.env.DOMAIN}${item.icon}`}
+                            />
+                          </div>
+                          <div className="title">{item.name}</div>
+                        </a>
+                      </div>
+                    );
+                  }
+                )}
+              </div>
+            </div>
+          </section>
+        </React.Fragment>
       )}
     </>
   );
