d6b6fb3d5822b2ef1b5636791b075b432b1d6e22
Linh Nguyen <linh.nguyen@miracles.vn>
2020-05-11 10:28:31 +0700

    fix data fb

--- components/common/Social.js
+++ components/common/Social.js
@@ -6,54 +6,57 @@ const propTypes = {
 };
 
 function Social({ data }) {
-  return (
-    <ul className="blog-item-social ">
-      <li>
-        <a
-          href={data.facebook}
-          className="item"
-          title="Facebook"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <i className="icon-facebook"></i>
-        </a>
-      </li>
-      <li>
-        <a
-          href={data.instagram}
-          className="item"
-          title="Instagram"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <i className="icon-instagram"></i>
-        </a>
-      </li>
-      <li>
-        <a
-          href={data.twitter}
-          className="item"
-          title="Twitter"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <i className="icon-twitter"></i>
-        </a>
-      </li>
-      <li>
-        <a
-          href={data.youtube}
-          className="item"
-          title="Youtube"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <i className="icon-youtube-2"></i>
-        </a>
-      </li>
-    </ul>
-  );
+  if (data) {
+    return (
+      <ul className="blog-item-social ">
+        <li>
+          <a
+            href={data.facebook}
+            className="item"
+            title="Facebook"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="icon-facebook"></i>
+          </a>
+        </li>
+        <li>
+          <a
+            href={data.instagram}
+            className="item"
+            title="Instagram"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="icon-instagram"></i>
+          </a>
+        </li>
+        <li>
+          <a
+            href={data.twitter}
+            className="item"
+            title="Twitter"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="icon-twitter"></i>
+          </a>
+        </li>
+        <li>
+          <a
+            href={data.youtube}
+            className="item"
+            title="Youtube"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="icon-youtube-2"></i>
+          </a>
+        </li>
+      </ul>
+    );
+  }
+  return null;
 }
 
 Social.propTypes = propTypes;
