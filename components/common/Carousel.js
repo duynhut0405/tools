0e712ef3e0c5d49233059e392e9156ca43817c0f
Linh Nguyen <linh.nguyen@miracles.vn>
2020-05-12 17:34:49 +0700

    fix slider black text

--- components/common/Carousel.js
+++ components/common/Carousel.js
@@ -3,6 +3,7 @@ import { map, isEmpty } from 'lodash';
 import PropTypes from 'prop-types';
 import ReactHtmlParser from 'react-html-parser';
 import { Carousel } from 'react-responsive-carousel';
+import ImgSliderBlack from '../../public/static/images/bgSliderback.jpg';
 
 const propType = {
   silder: PropTypes.array.isRequired,
@@ -13,17 +14,18 @@ function Carousels({ silder }) {
   return (
     <React.Fragment>
       {!isEmpty(silder) && (
-        <section className="silder banner-heading-1 home next-shadow">
-          <Carousel
-            showThumbs={false}
-            showStatus={false}
-            infiniteLoop={true}
-            emulateTouch
-            showIndicators={false}
-          >
-            {map(silder, (item, index) => {
-              if (item.options === '3') {
-                return (
+        <div>
+          {map(silder, (item, index) => {
+            if (item.options === '3') {
+              return (
+                <section
+                  className="  sec-dt-1  loaded"
+                  data-lazy-type="bg"
+                  data-lazy-src={ImgSliderBlack}
+                  style={{
+                    backgroundImage: `url("${ImgSliderBlack}")`
+                  }}
+                >
                   <div className={`container  option-${item.options}`} key={index}>
                     <div className="row center">
                       <div className="col-md-6">
@@ -47,53 +49,71 @@ function Carousels({ silder }) {
                       </div>
                     </div>
                   </div>
-                );
-              }
-              return (
-                <div className={`container  option-${item.options}`} key={index}>
-                  <div className="divtext text-left-mg ">
-                    <h1 className=" efch-2 ef-img-l">{ReactHtmlParser(item.note_1)}</h1>
-                    <div className="cl1">{ReactHtmlParser(item.note_2)}</div>
-                    <div className="text_content3">{item.note_3}</div>
-                    {item.text_action === null ||
-                    item.text_action === undefined ||
-                    item.text_action === '' ? (
-                      ''
-                    ) : (
-                      <a
-                        className="btn"
-                        href={
-                          item.url === null || item.url === undefined || item.url === ''
-                            ? '#'
-                            : item.url
-                        }
-                      >
-                        {item.text_action}
-                      </a>
-                    )}
-                    {item.video_url !== undefined && item.video_url !== '' && (
-                      <div>
-                        <iframe data-src={item.video_url} />
-                      </div>
-                    )}
-                  </div>
-                  <div className="wimg">
-                    <img
-                      className={item.options === '2' ? 'img-center img-pc' : 'img-right img-pc'}
-                      src={`${process.env.DOMAIN}${item.urlImage}`}
-                      alt="icon"
-                    />
-                    <img
-                      className={item.options === '2' ? 'img-center img-mb' : 'img-right img-mb'}
-                      src={`${process.env.DOMAIN}${item.urlImageMobile}`}
-                      alt="icon"
-                    />
-                  </div>
-                </div>
+                </section>
               );
-            })}
-          </Carousel>
-        </section>
+            }
+            return (
+              <section className="silder banner-heading-1 home next-shadow">
+                <Carousel
+                  showThumbs={false}
+                  showStatus={false}
+                  infiniteLoop={true}
+                  emulateTouch
+                  showIndicators={false}
+                >
+                  {map(silder, (item, index) => {
+                    return (
+                      <div className={`container  option-${item.options}`} key={index}>
+                        <div className="divtext text-left-mg ">
+                          <h1 className=" efch-2 ef-img-l">{ReactHtmlParser(item.note_1)}</h1>
+                          <div className="cl1">{ReactHtmlParser(item.note_2)}</div>
+                          <div className="text_content3">{item.note_3}</div>
+                          {item.text_action === null ||
+                          item.text_action === undefined ||
+                          item.text_action === '' ? (
+                            ''
+                          ) : (
+                            <a
+                              className="btn"
+                              href={
+                                item.url === null || item.url === undefined || item.url === ''
+                                  ? '#'
+                                  : item.url
+                              }
+                            >
+                              {item.text_action}
+                            </a>
+                          )}
+                          {item.video_url !== undefined && item.video_url !== '' && (
+                            <div>
+                              <iframe data-src={item.video_url} />
+                            </div>
+                          )}
+                        </div>
+                        <div className="wimg">
+                          <img
+                            className={
+                              item.options === '2' ? 'img-center img-pc' : 'img-right img-pc'
+                            }
+                            src={`${process.env.DOMAIN}${item.urlImage}`}
+                            alt="icon"
+                          />
+                          <img
+                            className={
+                              item.options === '2' ? 'img-center img-mb' : 'img-right img-mb'
+                            }
+                            src={`${process.env.DOMAIN}${item.urlImageMobile}`}
+                            alt="icon"
+                          />
+                        </div>
+                      </div>
+                    );
+                  })}
+                </Carousel>
+              </section>
+            );
+          })}
+        </div>
       )}
     </React.Fragment>
   );
