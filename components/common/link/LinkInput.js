fbeb378a6f8501087cc9cf9db5e300549217c62c
Linh Nguyen <linh.nguyen@miracles.vn>
2020-05-14 12:56:47 +0700

    add input url in block new

---
+++ components/common/link/LinkInput.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Link from 'next/link';
+
+import PropTypes from 'prop-types';
+
+const propTypes = {
+  lang: PropTypes.string,
+  name: PropTypes.string,
+  children: PropTypes.node
+};
+
+function LinkInput({ lang, name, children }) {
+  //
+  return (
+    <>
+      {lang === 'en' ? (
+        <Link href="/en/[...name]" as={`/${lang}/${name}`}>
+          {children}
+        </Link>
+      ) : (
+        <Link href="/[...name]" as={`/${name}`}>
+          {children}
+        </Link>
+      )}
+    </>
+  );
+}
+
+LinkInput.propTypes = propTypes;
+
+export default LinkInput;
