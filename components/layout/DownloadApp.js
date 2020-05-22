77605080383d516340c13b4ecd5130ebe0978ecc
Linh Nguyen <linh.nguyen@miracles.vn>
2020-05-22 17:32:32 +0700

    fix link app

--- components/layout/DownloadApp.js
+++ components/layout/DownloadApp.js
@@ -32,11 +32,11 @@ const DownloadApp = ({ mobile, linkApp }) => {
       {mobile && (
         <>
           <span className="stitle">{t('donwload_app_today')}</span>
-          <a href={linkApp.android} target="_blank" rel="noopener noreferrer">
+          <a href={linkApp.ios} target="_blank" rel="noopener noreferrer">
             <img className="lazyload" data-src="/static/images/btt-chplay-mb.svg" alt="images" />
           </a>
           &nbsp;
-          <a href={linkApp.ios} target="_blank" rel="noopener noreferrer">
+          <a href={linkApp.android} target="_blank" rel="noopener noreferrer">
             <img className="lazyload" data-src="/static/images/btt-google-mb.svg" alt="images" />
           </a>
         </>
