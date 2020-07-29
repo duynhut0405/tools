1000a9b387334f10f8c723f11dc36dd16a33b03a
Tran Lam <lam.tran@miracles.vn>
2020-07-29 23:47:20 +0700

    29.07.2020 ql fix_landing_page_01

--- components/block/formStepClient/componentModal/CaptionModal1.js
+++ components/block/formStepClient/componentModal/CaptionModal1.js
@@ -3,10 +3,13 @@ import React from 'react';
 export const CaptionModal1 = () => {
   return (
     <>
-      <p className="info1_text2">
+      <p
+        className="info1_text2"
+        style={{ fontWeight: 'bold', fontSize: '17px', marginBottom: '10px' }}
+      >
         Ngân hàng Quân đội (MB) trân trọng cảm ơn sự quan tâm của Quý Khách hàng
       </p>
-      <p className="info1_text2">
+      <p className="info1_text2" style={{ fontWeight: 'bold', fontSize: '17px' }}>
         Quý Khách hàng vui lòng lựa chon Chi nhánh cho vay hoặc Cán bộ tín dụng
       </p>
     </>
