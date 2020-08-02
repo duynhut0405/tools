632018bccad5c924d4f6ab3d74175cd7308899b5
Tran Lam <lam.tran@miracles.vn>
2020-08-02 21:51:17 +0700

    02.08.2020 ql lam_fix_land

--- components/block/formStepClient/ChildboxForm1.js
+++ components/block/formStepClient/ChildboxForm1.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+/* eslint-disable react/prop-types */
+import React, { useState, useEffect, useRef } from 'react';
 import Proptypes from 'prop-types';
 import BaseSelect from 'react-select';
 import FixRequiredSelect from './FixRequiredSelect';
@@ -9,14 +10,15 @@ const propTypes = {
   formState: Proptypes.object,
   item: Proptypes.object,
   index: Proptypes.number,
-  removeItem: Proptypes.func,
-  formikProps: Proptypes.object
+  removeItem: Proptypes.func
+  // formikProps: Proptypes.object
 };
 
 const ChildboxForm1 = ({ formState, setFormState, index, item, removeItem, formikProps }) => {
-  const [name, setName] = useState(item.name_componion);
-  const [relationship, setRelationship] = useState(item.rela_componion);
-  const [typeProfile, setTypeProfile] = useState(item.prof_componion);
+  const [name, setName] = useState(item.name_componion ? item.name_componion : '');
+  const [relationship, setRelationship] = useState(item.rela_componion ? item.rela_componion : '');
+  const [typeProfile, setTypeProfile] = useState(item.prof_componion ? item.prof_componion : '');
+  // const form02 = useRef(null);
 
   const checkExitItem = (id, list) => {
     if (list.filter(value => value.id === id).length > 0) {
@@ -24,6 +26,30 @@ const ChildboxForm1 = ({ formState, setFormState, index, item, removeItem, formi
     }
     return false;
   };
+
+  // useEffect(() => {
+  //   const htmlInput = document.getElementById('child01_name');
+  //   htmlInput.oninvalid = function(e) {
+  //     if (!e.target.validity.valid) {
+  //       e.target.setCustomValidity('Trường bắt buộc nhập');
+  //     }
+  //   };
+  // }, [name, relationship, typeProfile]);
+
+  // const formVal = e => {
+  //   if (name) {
+  //     e.target.setCustomValidity('');
+  //   } else {
+  //     e.target.setCustomValidity('Trường bắt buộc nhập');
+  //   }
+  // };
+
+  // const formVal1 = e => {
+  //   e.target.setCustomValidity('Trường bắt buộc nhập');
+  // };
+  // const formVal2 = e => {
+  //   e.target.setCustomValidity('Trường bắt buộc nhập');
+  // };
 
   const updateItem = (object, list) => {
     return list.map(value => {
@@ -41,7 +67,7 @@ const ChildboxForm1 = ({ formState, setFormState, index, item, removeItem, formi
   useEffect(() => {
     if (checkExitItem(item.id, formState.nuComponion)) {
       const object = {
-        id: item.id,
+        id: index,
         name_componion: name,
         rela_componion: relationship,
         prof_componion: typeProfile
@@ -62,7 +88,7 @@ const ChildboxForm1 = ({ formState, setFormState, index, item, removeItem, formi
       <span
         className="btn-close close-js"
         onClick={() => {
-          removeItem(index, formikProps);
+          removeItem(index);
         }}
       >
         <i className="icon-close"> </i>
@@ -76,38 +102,40 @@ const ChildboxForm1 = ({ formState, setFormState, index, item, removeItem, formi
             <input
               className="input"
               type="text"
-              name="name_element1"
-              required
+              name="name_componion"
               placeholder="Nhập đầy đủ họ tên vợ/ chồng"
-              defaultValue={item.name_componion}
+              value={name ? name : ''}
               onChange={e => {
                 setName(e.target.value);
-                formikProps.setFieldValue('name_element1', e.target.value);
+                formikProps.setFieldValue(`nuComponion.${index}.name_componion`, e.target.value);
               }}
               style={{ width: '100%' }}
+              required
             />
-            {formikProps.touched.name_element1 && formikProps.errors.name_element1 && (
-              <p className="red error">{formikProps.errors.name_element1}</p>
-            )}
           </div>
+          {formikProps.touched.nuComponion[index].name_componion &&
+            formikProps.errors.nuComponion[index].name_componion && (
+              <p className="red error">{formikProps.errors.nuComponion[index].name_componion}</p>
+            )}
           <div className="col-12 col-md-6">
             <h6 className="title1">
               Quan hệ (<span className="red">*</span>)
             </h6>
             <Select
               options={listPartner}
-              name="name_element1"
-              value={relationship}
+              name="name_element"
+              // id="name_input_1"
+              // onBlur={formVal1}
               required
-              defaultValue={item.rela_componion ? item.rela_componion : {}}
+              defaultValue={relationship ? relationship : {}}
               onChange={e => {
-                formikProps.setFieldValue('rela_element1', e);
+                // formVal(e);
                 setRelationship(e);
               }}
             />
-            {formikProps.touched.rela_element1 && formikProps.errors.rela_element1 && (
+            {/* {formikProps.touched.rela_element1 && formikProps.errors.rela_element1 && (
               <p className="red error">{formikProps.errors.rela_element1}</p>
-            )}
+            )} */}
           </div>
           <div className="col-12 col-md-6">
             <h6 className="title1">
@@ -116,8 +144,12 @@ const ChildboxForm1 = ({ formState, setFormState, index, item, removeItem, formi
             <input
               className="input"
               type="text"
+              id="name_input_2"
               placeholder="Nhập giấy tờ tùy thân"
-              onChange={e => setTypeProfile(e.target.value)}
+              onChange={e => {
+                // formVal(e);
+                setTypeProfile(e.target.value);
+              }}
               required
               style={{ width: '100%' }}
             />
