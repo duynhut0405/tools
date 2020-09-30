095a7c6f535f69a72b397d79ab90b2f36389f0db
Vu Dinh <phamnhuhuyhung@gmail.com>
2020-09-30 13:18:36 +0700

    fix repeat.js(table)

--- components/block/Repeat.js
+++ components/block/Repeat.js
@@ -44,35 +44,51 @@ function Repeat({ data, type, id, optionWidth }) {
   }
 
   if (type === '5') {
-    const col = 6;
+    const col = 5;
+    const [active, setAcive] = useState(false);
     return (
       <section className={`${padding} table-${type}`} id={id}>
         <div className="container">
           <div className="row list-item">
-            <div className="col-lg-7">
-              <div className="entry-head text-center" style={{borderBottom: "1px solid #ddd"}}>
-                <h2 style={{fontSize:"12px", color:"#686868", marginBottom: "10px"}}>{data.title}</h2>
-              </div>
-              <div className="row list-item grid-space-20">
-                {map(listTable, (item, index) => (
-                  <React.Fragment>
-                    <div className={`col-md-${col} `} key={index}>
-                      <div className="widget-default" style={{backgroundColor: item.backGroundColor}}>
-                        <div className="widget-content entry-content">
-                          <div
-                            className="toggleAutoHeight"
-                            data-more="+ Xem thêm"
-                            data-less="- Thu gọn"
-                            data-i=""
-                          >
-                            <h3 style={{fontSize: "18px"}}>{item.header}</h3>
-                            <TableItem data={item.description} maxheight={Number(data.height)} />
+            <div className={`accodion-tab`}>
+              <input
+                id={`checkbox_${id}`}
+                type="checkbox"
+                checked={active}
+                onClick={() => setAcive(!active)}
+              />
+              <label htmlFor={`checkbox_${id}`} className="accodion-title accodion-title-custom">
+                <span className="title-plus-custom">{data.title}</span>
+                <span className="triangle icon-plus-custom">
+                  <i className="icon-plus"></i>
+                </span>
+              </label>
+
+              <div className="accodion-content entry-content" style={{paddingTop: "20px"}}>
+                
+                <div className="row list-item grid-space-20">
+                  {map(listTable, (item, index) => (
+                    <React.Fragment>
+                      <div className={`col-md-${col} `} key={index}>
+                        <div className="widget-default" style={{backgroundColor: "#e0f0ff", WebkitBoxShadow: "none", boxShadow: "none"}}>
+                          <div className="widget-content entry-content">
+                            <div
+                              className="toggleAutoHeight"
+                              data-more="+ Xem thêm"
+                              data-less="- Thu gọn"
+                              data-i=""
+                            >
+                              <h3 style={{fontSize: "18px"}}>{item.header}</h3>
+                              <TableItem data={item.description} maxheight={Number(data.height)} />
+                            </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                  </React.Fragment>
-                ))}
+                    </React.Fragment>
+                  ))}
+                </div>
+
+
               </div>
             </div>
           </div>
@@ -83,34 +99,50 @@ function Repeat({ data, type, id, optionWidth }) {
 
   if (type === '6') {
     const col = 4;
+    const [active, setAcive] = useState(false);
     return (
       <section className={`${padding} table-${type}`} id={id}>
         <div className="container">
           <div className="row list-item">
-            <div className="col-lg-12">
-              <div className="entry-head text-center" style={{borderBottom: "1px solid #ddd"}}>
-                <h2 style={{fontSize:"12px", color:"#686868", marginBottom: "10px"}}>{data.title}</h2>
-              </div>
-              <div className="row list-item grid-space-20">
-                {map(listTable, (item, index) => (
-                  <React.Fragment>
-                    <div className={`col-md-${col} `} key={index}>
-                      <div className="widget-default" style={{backgroundColor: item.backGroundColor}}>
-                        <div className="widget-content entry-content">
-                          <div
-                            className="toggleAutoHeight"
-                            data-more="+ Xem thêm"
-                            data-less="- Thu gọn"
-                            data-i=""
-                          >
-                            <h3 style={{fontSize: "18px"}}>{item.header}</h3>
-                            <TableItem data={item.description} maxheight={Number(data.height)} />
+            <div className={`accodion-tab`}>
+              <input
+                id={`checkbox_${id}`}
+                type="checkbox"
+                checked={active}
+                onClick={() => setAcive(!active)}
+              />
+              <label htmlFor={`checkbox_${id}`} className="accodion-title accodion-title-custom">
+                <span className="title-plus-custom">{data.title}</span>
+                <span className="triangle icon-plus-custom">
+                  <i className="icon-plus"></i>
+                </span>
+              </label>
+
+              <div className="accodion-content entry-content" style={{paddingTop: "20px"}}>
+                
+                <div className="row list-item grid-space-20">
+                  {map(listTable, (item, index) => (
+                    <React.Fragment>
+                      <div className={`col-md-${col} `} key={index}>
+                        <div className="widget-default" style={{backgroundColor: "#e0f0ff", WebkitBoxShadow: "none", boxShadow: "none"}}>
+                          <div className="widget-content entry-content">
+                            <div
+                              className="toggleAutoHeight"
+                              data-more="+ Xem thêm"
+                              data-less="- Thu gọn"
+                              data-i=""
+                            >
+                              <h3 style={{fontSize: "18px"}}>{item.header}</h3>
+                              <TableItem data={item.description} maxheight={Number(data.height)} />
+                            </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                  </React.Fragment>
-                ))}
+                    </React.Fragment>
+                  ))}
+                </div>
+
+
               </div>
             </div>
           </div>
@@ -121,34 +153,141 @@ function Repeat({ data, type, id, optionWidth }) {
 
   if (type === '7') {
     const col = 3;
+    const [active, setAcive] = useState(false);
     return (
       <section className={`${padding} table-${type}`} id={id}>
         <div className="container">
           <div className="row list-item">
-            <div className="col-lg-12">
-              <div className="entry-head text-center" style={{borderBottom: "1px solid #ddd"}}>
-                <h2 style={{fontSize:"12px", color:"#686868", marginBottom: "10px"}}>{data.title}</h2>
-              </div>
-              <div className="row list-item grid-space-20">
-                {map(listTable, (item, index) => (
-                  <React.Fragment>
-                    <div className={`col-md-${col} `} key={index}>
-                      <div className="widget-default" style={{backgroundColor: item.backGroundColor}}>
-                        <div className="widget-content entry-content">
-                          <div
-                            className="toggleAutoHeight"
-                            data-more="+ Xem thêm"
-                            data-less="- Thu gọn"
-                            data-i=""
-                          >
-                            <h3 style={{fontSize: "18px"}}>{item.header}</h3>
-                            <TableItem data={item.description} maxheight={Number(data.height)} />
+            <div className={`accodion-tab`}>
+              <input
+                id={`checkbox_${id}`}
+                type="checkbox"
+                checked={active}
+                onClick={() => setAcive(!active)}
+              />
+              <label htmlFor={`checkbox_${id}`} className="accodion-title accodion-title-custom">
+                <span className="title-plus-custom">{data.title}</span>
+                <span className="triangle icon-plus-custom">
+                  <i className="icon-plus"></i>
+                </span>
+              </label>
+
+              <div className="accodion-content entry-content" style={{paddingTop: "20px"}}>
+                <div className="row list-item grid-space-20">
+                  {map(listTable, (item, index) => (
+                    <React.Fragment>
+                      <div className={`col-md-${col} `} key={index}>
+                        <div className="widget-default" style={{backgroundColor: "#e0f0ff", WebkitBoxShadow: "none", boxShadow: "none"}}>
+                          <div className="widget-content entry-content">
+                            <div
+                              className="toggleAutoHeight"
+                              data-more="+ Xem thêm"
+                              data-less="- Thu gọn"
+                              data-i=""
+                            >
+                              <h3 style={{fontSize: "18px"}}>{item.header}</h3>
+                              <TableItem data={item.description} maxheight={Number(data.height)} />
+                            </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                  </React.Fragment>
-                ))}
+                    </React.Fragment>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+
+  if (type === '8') {
+    let col = data.length === 2 ? 6 : 4;
+    if (data.length === 4) {
+      col = 3;
+    }
+    const [active, setAcive] = useState(false);
+    return (
+      <section className={`${padding} table-${type}`} id={id}>
+        <div className="container">
+          <div className="row list-item">
+            <div className={`accodion-tab`}>
+              <input
+                id={`checkbox_${id}`}
+                type="checkbox"
+                checked={active}
+                onClick={() => setAcive(!active)}
+              />
+              <label htmlFor={`checkbox_${id}`} className="accodion-title accodion-title-custom">
+                <span className="title-plus-custom">{data.title}</span>
+                <span className="triangle icon-plus-custom">
+                  <i className="icon-plus"></i>
+                </span>
+              </label>
+
+              <div className="accodion-content entry-content" style={{paddingTop: "20px"}}>
+                {active ? 
+                  <div className="row list-item grid-space-20">
+                    {map(listTable, (item, index) => (
+                      <React.Fragment>
+                        <div className={`col-md-${col} `} key={index}>
+                          <div className="widget-default">
+                            <h4 className="widget-title">{item.header}</h4>
+                            <div className="widget-content entry-content">
+                              <div
+                                className="toggleAutoHeight"
+                                data-more="+ Xem thêm"
+                                data-less="- Thu gọn"
+                                data-i=""
+                              >
+                                <TableItem data={item.description} maxheight={Number(data.height)} />
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </React.Fragment>
+                    ))}
+                  </div> 
+                  : null
+                }
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+
+  if (type === '9') {
+    const [active, setAcive] = useState(false);
+    return (
+      <section className={`${padding} table-${type}`} id={id}>
+        <div className="container">
+          <div className="row list-item">
+            <div className={`accodion-tab`}>
+              <input
+                id={`checkbox_${id}`}
+                type="checkbox"
+                checked={active}
+                onClick={() => setAcive(!active)}
+              />
+              <label htmlFor={`checkbox_${id}`} className="accodion-title accodion-title-custom">
+                <span className="title-plus-custom">{data.title}</span>
+                <span className="triangle icon-plus-custom">
+                  <i className="icon-plus"></i>
+                </span>
+              </label>
+
+              <div className="accodion-content entry-content" style={{paddingTop: "20px"}}>
+                {active ? 
+                  <div>
+                    {map(listTable, (items, index) => {
+                      return <div key={index}> {ReactHtmlParser(items.description)}</div>;
+                    })}
+                  </div> 
+                  : null
+                }
               </div>
             </div>
           </div>
