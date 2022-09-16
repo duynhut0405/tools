6d5956495898efd65391dc880ac878d377ea241c
nhựt nguyễn <duynhut0405@gmail.com>
2022-09-16 15:58:19 +0700

    add block

--- components/tailwind/index.js
+++ components/tailwind/index.js
@@ -2,4 +2,7 @@ import Landing1 from './Landing1';
 import Landing2 from './Landing2';
 import Landing3 from './Landing3';
 import Contact from './Contact';
-export { Landing1, Landing2, Landing3, Contact };
+import Content1 from './Content1';
+import Content2 from './Content2';
+import Content3 from './Content3';
+export { Landing1, Landing2, Landing3, Contact, Content1, Content2, Content3 };
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { Carousel, BlockRender, MenuMiddle, Breadcrumb } from '../components/common';
+import { Carousel, BlockRender, MenuMiddle, Breadcrumb, Contact } from '../components/common';
 import Head from 'next/head';
 import Cookies from 'js-cookie';
-
+import { Content1, Content2, Content3 } from '../components/tailwind';
 function Home({}) {
   return (
     <React.Fragment>
       <div className="main_content">
-        <p className="text-xl">asdasasd as das ddas</p>
+        <Content1 />
+        <Content2 />
+        <Content3 />
       </div>
     </React.Fragment>
   );
