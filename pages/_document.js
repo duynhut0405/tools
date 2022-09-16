b58c0bdb4d639053bcbaad9041148e099a5f8f32
nhựt nguyễn <duynhut0405@gmail.com>
2022-09-16 13:56:52 +0700

    add gtm

---
+++ pages/_document.js
@@ -0,0 +1,35 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="shortcut icon" href="/favicon/favicon.ico" type="image/x-icon"></link>
+          <link rel="stylesheet" href="/css/main.css?ver=1" type="text/css" media="all"></link>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TCM7LJD');`
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TCM7LJD"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+            }}
+          />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
